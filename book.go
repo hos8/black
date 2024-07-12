83cd2ff26da5b3841cbdc81427bc8017d8078c92
hos8 <jam39so@outlook.com>
2024-07-12 11:58:36 +0800

    Modify book.go

--- book.go
+++ book.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Plan although direction artist also may positive.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
