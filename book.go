a763b14b407ceb908a9ba357d3cdfa743283633a
hos8 <jam39so@outlook.com>
2024-07-13 15:59:06 +0800

    Modify book.go

--- book.go
+++ book.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Series first over hear pass could campaign.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
