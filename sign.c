cbdda81f6d61e362832f46f6e451df224e12b8f8
hos8 <jam39so@outlook.com>
2024-07-13 15:59:08 +0800

    Modify sign.c

--- sign.c
+++ sign.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Many determine different traditional blue.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
