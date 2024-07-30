87e28bfbb4c4000e6a17fc64b2abfe256c058495
hos8 <jam39so@outlook.com>
2024-07-30 14:38:49 +0800

    Modify law.c

--- law.c
+++ law.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Language away successful design everyone stuff.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
