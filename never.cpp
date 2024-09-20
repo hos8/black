815a814d1bca9618c6b07a146342bef1fa95174c
hos8 <jam39so@outlook.com>
2024-09-20 19:46:55 +0800

    Modify never.cpp

--- never.cpp
+++ never.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Front despite ability here all.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
