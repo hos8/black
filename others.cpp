17cddb77dbb6324ff3d4ec2b89a8fa046218f12b
hos8 <jam39so@outlook.com>
2024-09-29 13:39:06 +0800

    Modify others.cpp

--- others.cpp
+++ others.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Strategy to customer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
