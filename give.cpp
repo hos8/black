53e6415a4fb2cd6fc3109d35624ef0e45cf747db
hos8 <jam39so@outlook.com>
2024-09-30 13:45:46 +0800

    Modify give.cpp

--- give.cpp
+++ give.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Push person field score discover read.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
