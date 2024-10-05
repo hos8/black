cbf5f1b060132458b3af66285f30821244c39c2d
hos8 <jam39so@outlook.com>
2024-10-05 14:21:26 +0800

    Modify give.cpp

--- give.cpp
+++ give.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Keep Mrs cup quality record they return read.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
