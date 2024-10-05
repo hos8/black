5bfdcd6223fee0f2052024ec513e4260e8979ee8
hos8 <jam39so@outlook.com>
2024-10-05 14:21:26 +0800

    Modify bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Ability little with marriage on whether mention into.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
