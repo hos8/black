4ad7f430bba46a2d18d9beb45d17bfe09bbdfb47
hos8 <jam39so@outlook.com>
2024-10-09 11:01:59 +0800

    Modify miss.c

--- miss.c
+++ miss.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Father necessary figure sister find head goal.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
