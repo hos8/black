cd9b3c94ad4dcb4ae824f656ff4b1326d3398664
hos8 <jam39so@outlook.com>
2024-10-10 13:21:02 +0800

    Modify bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -17,3 +17,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Very night end law theory store beat.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
