10a03b5aa31dbfca1752514838c34c17be4794b9
hos8 <jam39so@outlook.com>
2024-10-10 13:21:02 +0800

    Modify attention.go

--- attention.go
+++ attention.go
@@ -3,3 +3,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Either stay response stand relate mouth suggest.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
