6b70460caf4274751288c16623d21f6d9f232c2e
hos8 <jam39so@outlook.com>
2024-10-12 10:13:01 +0800

    Modify miss.c

--- miss.c
+++ miss.c
@@ -17,3 +17,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Write southern art business whatever range certainly.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
