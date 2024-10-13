ff206c2020e3bc08d95e3f6cf1f48895985b7461
hos8 <jam39so@outlook.com>
2024-10-13 12:03:49 +0800

    Modify bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -24,3 +24,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Company economic dinner explain case rise.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
