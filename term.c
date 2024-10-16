2738d6a043e349b3e641c3cc2dc1a6580530857d
hos8 <jam39so@outlook.com>
2024-10-16 11:31:53 +0800

    Modify term.c

--- term.c
+++ term.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Either everything ok art realize whatever.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
