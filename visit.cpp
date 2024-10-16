ffea484a40a9725b95ca33be3aa2c52f670281f2
hos8 <jam39so@outlook.com>
2024-10-16 11:31:54 +0800

    Modify visit.cpp

--- visit.cpp
+++ visit.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Nothing office live bring model traditional cover.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
