1bf6a56db80cefc32a59b8c33101949b422a8603
hos8 <jam39so@outlook.com>
2024-10-21 11:43:34 +0800

    Modify bar.cpp

--- bar.cpp
+++ bar.cpp
@@ -31,3 +31,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Maybe trip school firm change.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
