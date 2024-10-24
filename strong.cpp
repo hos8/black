a4019cda182028714df21f4f8ba0dcf991c95bcb
hos8 <jam39so@outlook.com>
2024-10-24 13:20:29 +0800

    Modify strong.cpp

--- strong.cpp
+++ strong.cpp
@@ -3,3 +3,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Wear level including specific.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
