891f405e430387c903e86273eb72e5cf29a98ac2
hos8 <jam39so@outlook.com>
2024-11-08 11:46:07 +0800

    Modify must.c

--- must.c
+++ must.c
@@ -3,3 +3,10 @@ int main() {
 	printf("Hello, World!\n");
 	return 0;
 }
+
+# Executive environment player assume successful before too spring.
+#include <stdio.h>
+int main() {
+	printf("Hello, World!\n");
+	return 0;
+}
