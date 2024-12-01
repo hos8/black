c4e2ef94bca7a4acd78db76844931f594f1bd51d
hos8 <jam39so@outlook.com>
2024-12-02 00:23:11 +0800

    Modify himself.cpp

--- himself.cpp
+++ himself.cpp
@@ -10,3 +10,10 @@ int main() {
 	std::cout << "Hello, World!" << std::endl;
 	return 0;
 }
+
+# Later time sense at coach nothing suffer.
+#include <iostream>
+int main() {
+	std::cout << "Hello, World!" << std::endl;
+	return 0;
+}
