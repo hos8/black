2d4e15099e82f01a2b63ffc063c79a4db8a56da0
hos8 <jam39so@outlook.com>
2025-02-21 11:45:33 +0800

    Modify card.go

--- card.go
+++ card.go
@@ -10,3 +10,10 @@ import "fmt"
 func main() {
 	fmt.Println("Hello, World!")
 }
+
+# Occur you agree student information institution subject I.
+package main
+import "fmt"
+func main() {
+	fmt.Println("Hello, World!")
+}
